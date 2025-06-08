5a31f0b93b16fc1efa1ca671e110fb2795f86fdf
Ignacio Esquivel <jose.i.audeves@gmail.com>
2025-06-08 06:01:10 -0700

    refactor Project interfaces: restructure to enhance code clarity and maintainability

--- task-manager-frontend/front_angular/src/app/models/project.ts
+++ task-manager-frontend/front_angular/src/app/models/project.ts
@@ -1,4 +1,9 @@
-export interface Project {
-    id: string
-    name: string
+export interface ProjectBase {
+    name: string;
+}
+
+export interface ProjectPayload extends ProjectBase {}
+
+export interface Project extends ProjectBase {
+    id: string;
 }
